7db21a3f9e4667916b14c06cb400ce078c9198bd
carole tierney <caroleatierney@gmail.com>
2025-11-05 19:16:42 -0500

    changed DB to new db on render in postgres.js

--- server/postgres.js
+++ server/postgres.js
@@ -1,7 +1,7 @@
 const Client = require('pg').Client
 
 const client = new Client ({
-  connectionString: process.env.DATABASE_URL || 'postgresql://localhost:5432/marbeachrec',
+  connectionString: process.env.DATABASE_URL || 'postgresql://localhost:5432/MarshVegasBeaches',
 })
 
 module.exports = client;
