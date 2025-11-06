630043b03531151915fe51dd1aee9752898f8a66
carole tierney <caroleatierney@gmail.com>
2025-11-05 19:18:54 -0500

    added suggestions to postgres.js

--- server/postgres.js
+++ server/postgres.js
@@ -1,7 +1,14 @@
-const Client = require('pg').Client
+const { Client } = require("pg");
 
-const client = new Client ({
-  connectionString: process.env.DATABASE_URL || 'postgresql://localhost:5432/MarshVegasBeaches',
-})
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || "postgresql://localhost:5432/MarshVegasBeaches",
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
+
+client.connect();
 
 module.exports = client;
