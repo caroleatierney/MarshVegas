d735e27a31a5b4b4cf022a5497e451c5887dd862
carole tierney <caroleatierney@gmail.com>
2025-11-11 18:54:55 -0500

    added message to /

--- server/server.js
+++ server/server.js
@@ -14,4 +14,9 @@ app.use("/marshVegasBeaches", marshVegasBeachesController);
 // ==================== LISTENER ====================
 // ==================================================
 const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("API is running. Try /marshVegasBeaches");
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
