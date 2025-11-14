f7cf6db280c05a5a25ffa5d90edb125b40a05b84
carole tierney <caroleatierney@gmail.com>
2025-11-14 18:00:31 -0500

    add postgres command to MV server

--- server/routes/marshVegasBeaches.js
+++ server/routes/marshVegasBeaches.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const postgres = require("../postgres.js");
 const Beaches = require("../models/marshVegasBeaches");
 
 // GET all beaches
