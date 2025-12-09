73e7f3ea1eb56af0e398bcc710affcdf9ae83bc8
carole tierney <caroleatierney@gmail.com>
2025-12-09 13:17:30 -0500

    Fixed start up scripts and moved Bulm in src

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import "bulma/css/bulma.min.css";
-
-import "../my-bulma-project.css";
-import "../my-bulma-project.scss";
+import "./my-bulma-project.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
