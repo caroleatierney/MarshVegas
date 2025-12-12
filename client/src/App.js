2af60545b66160006bd14b3f819d2cafea792af6
carole tierney <caroleatierney@gmail.com>
2025-12-12 15:18:30 -0500

    added tide logic

--- client/src/App.js
+++ client/src/App.js
@@ -16,11 +16,116 @@ class App extends React.Component {
     },
     editBeachId: null,
     editData: {},
+    // tide results:
+    high1: "",
+    highDate1: "",
+    highTime1: "",
+    highHeight1: "",
+    low1: "",
+    lowDate1: "",
+    lowTime1: "",
+    lowHeight1: "",
+    high2: "",
+    highDate2: "",
+    highTime2: "",
+    highHeight2: "",
+    low2: "",
+    lowDate2: "",
+    lowTime2: "",
+    lowHeight2: "",
   };
 
   componentDidMount() {
     this.loadBeaches();
   }
+
+  //***********************************************
+  //**************** GET TIDES ********************
+  //***********************************************
+  getTides = (event) => {
+    // console.log(this.state.lat);
+    // console.log(this.state.long);
+
+    fetch(
+      "https://tides.p.rapidapi.com/tides?latitude=" +
+        this.state.lat +
+        "&longitude=" +
+        this.state.long,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "tides.p.rapidapi.com",
+          "x-rapidapi-key":
+            "72f2d4d192mshd7feaecf8ffd802p140faajsna045792ab384",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data || !data.extremes || data.extremes.length === 0) {
+          console.error("No tide data returned:", data);
+          return;
+        }
+
+        // Make sure we don’t try to read more tides than exist
+        const count = Math.min(4, data.extremes.length);
+        const date = [];
+        const time = [];
+        const height = [];
+
+        for (let i = 0; i < count; i++) {
+          const entry = data.extremes[i];
+          if (!entry) continue;
+
+          let extremeNewDate = new Date(entry.datetime);
+          let extremeTime = extremeNewDate.toLocaleTimeString();
+          let extremeDate = extremeNewDate.toLocaleDateString();
+          let extremeHeight = entry.height;
+          let extremeRoundedHeight = Math.round(extremeHeight * 100) / 100;
+
+          date[i] = extremeDate;
+          time[i] = extremeTime;
+          height[i] = extremeRoundedHeight;
+        }
+
+        this.setState({
+          high1: data.extremes[0]?.state || "",
+          highDate1: date[0] || "",
+          highTime1: time[0] || "",
+          highHeight1: height[0] || "",
+
+          low1: data.extremes[1]?.state || "",
+          lowDate1: date[1] || "",
+          lowTime1: time[1] || "",
+          lowHeight1: height[1] || "",
+
+          high2: data.extremes[2]?.state || "",
+          highDate2: date[2] || "",
+          highTime2: time[2] || "",
+          highHeight2: height[2] || "",
+
+          low2: data.extremes[3]?.state || "",
+          lowDate2: date[3] || "",
+          lowTime2: time[3] || "",
+          lowHeight2: height[3] || "",
+        });
+      });
+
+        // console.log(data);
+        // console.log(data.extremes);
+        // console.log(data.extremes[0].datetime);
+
+  
+          // console.log(i);
+          // console.log(extremeNewDate);
+          // console.log(extremeDate);
+          // console.log(extremeTime);
+          // console.log(extremeRoundedHeight);
+
+
+
+  
+  };
 
   loadBeaches = () => {
     fetchBeaches()
@@ -118,7 +223,7 @@ class App extends React.Component {
                     <em>Photo Credit: {beach.photo_credit}</em>
                   </p>
 
-                  <div className="list">
+                  <div className="list-col">
                     <details>
                       <summary>Parking Details</summary>
                       <ul className="list">
@@ -129,8 +234,8 @@ class App extends React.Component {
                     </details>
 
                     <details>
-                      <summary>Hours</summary>
-                      <ul>
+                      <summary>General Hours and Access</summary>
+                      <ul className="list-col">
                         {hoursArray.map((p, idx) => (
                           <li key={idx}>{p}</li>
                         ))}
@@ -147,9 +252,63 @@ class App extends React.Component {
                         ))}
                       </ul>
                     </details>
+
+                    <details>
+                        <summary
+                          onClick={() => {
+                            this.setState(
+                              { lat: beach.latitude, long: beach.longitude },
+                              this.getTides
+                            );
+                          }}
+                        >
+                          tides
+                     </summary>
+
+                    <div>
+                        <table border="1">
+                          <thead>
+                            <tr key={beach.id}>
+                              <th>Extreme</th>
+                              <th>Date</th>
+                              <th>Time</th>
+                              <th>Height</th>
+                            </tr>
+                          </thead>
+
+                          <tbody>
+                            <tr>
+                              <td> {this.state.high1} </td>
+                              <td> {this.state.highDate1} </td>
+                              <td> {this.state.highTime1} </td>
+                              <td> {this.state.highHeight1} </td>
+                            </tr>
+                            <tr>
+                              <td> {this.state.low1} </td>
+                              <td> {this.state.lowDate1} </td>
+                              <td> {this.state.lowTime1} </td>
+                              <td> {this.state.lowHeight1} </td>
+                            </tr>
+                            <tr>
+                              <td> {this.state.high2} </td>
+                              <td> {this.state.highDate2} </td>
+                              <td> {this.state.highTime2} </td>
+                              <td> {this.state.highHeight2} </td>
+                            </tr>
+                            <tr>
+                              <td> {this.state.low2} </td>
+                              <td> {this.state.lowDate2} </td>
+                              <td> {this.state.lowTime2} </td>
+                              <td> {this.state.lowHeight2} </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <p>Not suitable for navigation purposes</p>
+                      </div>
+                    </details>
                   </div>
 
-                  <p>Notes: {beach.notes}</p>
+                  <p>{beach.notes}</p>
 
                   <div className="buttons equal-buttons">
                     <button
