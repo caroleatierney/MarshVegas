60dc3ed75023d9e9c9b0ec6703424193e5d0bad2
carole tierney <caroleatierney@gmail.com>
2026-04-25 10:58:23 -0400

    Add Tides Route

--- server/routes/beaches.js
+++ server/routes/beaches.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db");
 
+// Health check endpoint
+router.get("/health", (req, res) => {
+  res.json({ ok: true });
+});
+
 // GET all beaches
 router.get("/", async (req, res) => {
   try {
@@ -38,6 +43,39 @@ router.post("/", async (req, res) => {
   } catch (err) {
     console.error("POST /beaches error", err);
     res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.get("/tides", async (req, res) => {
+  try {
+    const { lat, long } = req.query;
+
+    const apiKey = process.env.STORMGLASS_API_KEY;
+
+    if (!apiKey) {
+      return res.status(500).json({ error: "Missing Stormglass API key" });
+    }
+
+    const today = new Date();
+    const yyyyMmDd = today.toISOString().split("T")[0];
+
+    const start = `${yyyyMmDd}T00:00:00Z`;
+    const end = `${yyyyMmDd}T23:59:59Z`;
+
+    const response = await fetch(
+      `https://api.stormglass.io/v2/tide/extremes/point?lat=${lat}&lng=${long}&start=${start}&end=${end}`,
+      {
+        headers: {
+          Authorization: apiKey,
+        },
+      },
+    );
+
+    const data = await response.json();
+    return res.status(response.status).json(data);
+  } catch (err) {
+    console.error("GET /beaches/tides error", err);
+    res.status(500).json({ error: "Failed to fetch tides" });
   }
 });
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const fetch = require("node-fetch");
 const app = express();
 app.use(express.json());
 
